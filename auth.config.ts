7ad87b754885d960e1755598e3423aadcd5b70b5
Nicolas Valdes <valdes.nicoedu@gmail.com>
2024-02-04 22:08:43 -0300

    next-auth-complete

---
+++ auth.config.ts
@@ -0,0 +1,34 @@
+import type { NextAuthConfig } from "next-auth";
+
+import credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import bcrypt from "bcryptjs";
+
+import github from "next-auth/providers/github";
+import google from "next-auth/providers/google";
+
+export default {
+  providers: [
+    github,
+    google,
+    credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
---
+++ route.ts
@@ -0,0 +1,28 @@
+/**
+ * an array of routes that are accesible to the public
+ * those routes do not require authentication
+ * @type {string[]}
+ */
+export const PublicRoutes = ["/"];
+
+/**
+ * an array of routes that are used for authentication
+ * those routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * the prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+
+export const apiAuthPrefix = "api/auth";
+
+/**
+ * the default redirect path after logged in
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
